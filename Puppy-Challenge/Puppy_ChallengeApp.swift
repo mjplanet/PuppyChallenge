029cb36b4c114c68ac5f30ab7e466cb4b31e41a1
Mobin Jahantark <jahantark@me.com>
2022-11-28 13:12:23 +0330

    Initial Commit

---
+++ Puppy-Challenge/Puppy_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puppy_ChallengeApp.swift
+//  Puppy-Challenge
+//
+//  Created by Mobin Jahantark on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Puppy_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
